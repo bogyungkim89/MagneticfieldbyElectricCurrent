f60bff1b35863ec86414ea5b69e9adc50507f80c
Bogyung Kim <bogyungkim89@gmail.com>
2025-08-31 13:49:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if simulation_type == "직선 전류":
     # 제목 변경
     st.header("1. 직선 전류에 의한 자기장")
     st.markdown("수직 도선에 흐르는 전류와 주변에 형성되는 자기장을 3차원으로 보여줍니다.")
-    st.markdown("<span style='font-size: 140%;'>$B=k(I/r)$</span>", unsafe_allow_html=True)
+    st.markdown("<span style='font-size: 140%;'>$B=k \\frac{I}{r)$</span>", unsafe_allow_html=True)
 
     # 사용자 입력 (슬라이더)
     I = st.slider("전류의 세기 (I)", 0.1, 5.0, 2.0, help="자기장의 세기와 전류 선의 굵기에 영향을 줍니다.")
